972b49b73d132361e7b4c57703b8a91eae8cb93a
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-02-19 19:30:20 +0200

    feat: configured axios

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF8C42",
+        secondary: "#FFA500",
+        dark: "#1a1a1a",
+      },
+    },
   },
   plugins: [],
-}
-
+};
