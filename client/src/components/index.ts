879e3488513b742eb2b737a7a2ea1e242a97d046
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-02-25 14:42:15 +0200

    feat: add components barrel export file

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Input } from './Input';
+export { default as ErrorAlert } from './ErrorAlert';
+export { default as BackButton } from './BackButton';
+export { default as LoadingSpinner } from './LoadingSpinner';
