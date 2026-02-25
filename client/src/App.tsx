33b42472159d8c74c82610b1e1cbc890d47ae154
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-02-25 23:27:55 +0200

    added routes for about and contact pages and footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,24 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import VendorDashboard from "./pages/VendorDashboard";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/vendor-dashboard" element={<VendorDashboard />} />
-      </Routes>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex flex-col">
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/vendor-dashboard" element={<VendorDashboard />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
